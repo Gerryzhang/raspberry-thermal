7e6d74dab8d0bbe3a92ea29d8ba7e37c1da2c8b7
zhangruihua <zhangruihua@mininglamp.com>
2019-12-16 15:21:15 +0800

    [Modify]更新打印格式

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
 import time
+
 while True:
-    #每间隔2s执行一次
+    with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
+        # 打印当前温度
+        print("当前温度%.2f" % (int(f.read()) / 1000))
+    # 每间隔2s执行一次
     time.sleep(2)
-    with open('/sys/class/thermal/thermal_zone0/temp','r') as f:
-        #打印当前温度
-        print(int(f.read()) / 1000)
