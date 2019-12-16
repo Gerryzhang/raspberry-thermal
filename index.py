c5e69139d2f1d4ffb9afa7ee9600acb64432129a
zhangruihua <zhangruihua@mininglamp.com>
2019-12-16 15:06:27 +0800

    [Modify]更新index.py

--- index.py
+++ index.py
@@ -2,6 +2,6 @@ import time
 while True:
     #每间隔2s执行一次
     time.sleep(2)
-    with open('/sys/class/thermal/thermal_zon0/temmp','r') as f:
+    with open('/sys/class/thermal/thermal_zone0/temp','r') as f:
         #打印当前温度
         print(int(f.read()) / 1000)
